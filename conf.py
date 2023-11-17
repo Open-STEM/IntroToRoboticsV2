1cdc3830321338b91cd3e43e64285274676e6dd1
bradamiller <brad@bradhouse.com>
2023-11-17 08:51:46 -0500

    moved navigation buttons to top for Canvas

--- conf.py
+++ conf.py
@@ -30,5 +30,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'prev_next_buttons_location': 'top'
+}
 html_static_path = ['_static']
 master_doc = 'index'
